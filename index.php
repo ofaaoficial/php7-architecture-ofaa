9f5dac14cd8c7aa129f09c999a40e80610a4cca1
ofaaoficial <oscarfamado@gmail.com>
2019-09-18 14:24:49 -0500

    start project with basic configuration of architecture MVC

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    $controller = isset($_GET['controller']) ? $_GET['controller'] : 'index';
+    $method = isset($_GET['method']) ? $_GET['method'] : 'index';
+
+    $controller .= 'Controller';
+
+    if(method_exists($controller, $method)){
+        $controller = new $controller;
+        call_user_func([$controller, $method]);
+    }else{
+        die("The method [{$method}] no exists in [{$controller}].");
+    }
